d271c805b338d405a94c4c6d556a28aa19d6328b
shawn <observer_mirror@yahoo.co.uk>
2014-05-02 11:32:59 +0800

    testing 20140502

--- hello.php
+++ hello.php
@@ -4,4 +4,6 @@ echo "Hello the Movie!!"
 echo "Change 1st time!!"
 echo "Override instead of update directly - Testing"
 echo "update in Pacific coffee!"
+echo " from shawn again , try delete all file and clone again "
+echo " Benny , can you see this ?"
 ?>
